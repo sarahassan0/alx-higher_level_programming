420cd0f17653550984a91dba01ad034ce3230396
sarah hassan <sarraree5.gmail.com>
2023-06-06 23:18:36 +0300

    Update - f

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,4 @@ for i in range(122, 96, -1):
     if(i % 2 == 0):
         print("{:c}".format(i), end="")
     else:
-        print("{:c}".format(i-32), end="")
-            
+        print("{:c}".format(i-32), end="")      
