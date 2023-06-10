b93b17cc0f9be98db154fd50ad3d64c58580c086
sarah hassan <sarraree5.gmail.com>
2023-06-11 02:35:46 +0300

    Update - task 4

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix != None:
+        for row in matrix:
+            for num in row:
+                print("{:d}".format(num), end=" " if num != row[-1] else "")
+            print()
