6e4ff52f47b2a0f72912361ae26b994982dbeade
sarah hassan <sarraree5.gmail.com>
2023-06-11 19:38:39 +0300

    Update - task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,5 +4,5 @@ def divisible_by_2(my_list=[]):
     if my_list is not None:
         list = []
         for i in my_list:
-            list[i] = True if (i % 2 == 0) else False
-            return list
+            list.append((i % 2) == 0)
+        return list
