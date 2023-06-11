f845db6f906971ca95378995f20e314500350e31
sarah hassan <sarraree5.gmail.com>
2023-06-11 19:13:51 +0300

    Update - task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence is not None:
+    if sentence == "":
+        return (0, None)
+    else:
         tuple = (len(sentence), sentence[0])
-
         return tuple
