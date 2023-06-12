eb8cf2500f1d9a4295462f963cc77223c8d03ef6
sarah hassan <sarraree5.gmail.com>
2023-06-12 23:29:42 +0300

    Update -

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -18,6 +18,12 @@ void print_python_list_info(PyObject *p)
     for (i = 0; i < size; i++)
     {
         printf("Element %d: ", i);
+        /*
+        these 2 line can be repleced with
+
+        PyListObject obj = (PyListObject *)p
+        printf("%s\n", Py_TYPE(obj->obj_item[i])->tp_name);
+          */
         obj = PyList_GetItem(p, i);
         printf("%s\n", Py_TYPE(obj)->tp_name);
     }
