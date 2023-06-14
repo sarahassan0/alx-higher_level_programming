e5b2bbc840439e1923f061b73a84cc224f82e253
sarah hassan <sarraree5.gmail.com>
2023-06-15 00:34:36 +0300

    Update - tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
-#!/usr/bin/pythin3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # [[num**2 for num in x] for x in matrix]
-    return [list(map(lambda num: num * num, x)) for x in matrix]
+    return [list(map((lambda x: x * x), elm)) for elm in matrix]
