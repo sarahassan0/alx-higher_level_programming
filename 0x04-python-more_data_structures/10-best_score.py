b2ba9b07f87166b401e3a3588842ea6e354efc3a
sarah hassan <sarraree5.gmail.com>
2023-06-14 22:23:00 +0300

    Update - tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     else:
         max = sorted(a_dictionary.values())[-1]
