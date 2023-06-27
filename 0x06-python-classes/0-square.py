095ebd835985c18d8f6400f89f4f6f7b314c227c
sarahassan0 <sarraree5@gmail.com>
2023-06-27 15:04:15 +0300

    edit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,4 @@
-#!usr/bin/python
+#!usr/bin/python3
+
 class Square:
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,5 @@
-#!usr/bin/python
+#!usr/bin/python3
+
 class Square:
     def __init__(self, size):
         self._size = size
