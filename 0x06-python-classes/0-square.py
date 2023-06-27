d5ebe190e19ae6a8541ca0041c4146e81f33f7ec
sarahassan0 <sarraree5@gmail.com>
2023-06-27 15:16:34 +0300

    decomenting

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!usr/bin/python3
 
+""" This module represents A Square class """
+
 class Square:
+    """ A square class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!usr/bin/python3
 
+""" This module represents A Square class """
+
 class Square:
+    """ A square class"""
+
     def __init__(self, size):
+        """
+        init function initalizes the square.
+
+        Args:
+            size (int): The size of a square.
+        """
         self._size = size
