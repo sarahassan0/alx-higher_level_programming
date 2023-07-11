e109b049eb974ff95e41926c5f7e3c3b396ee218
sarahassan0 <sarraree5@gmail.com>
2023-07-11 21:36:47 +0300

    Update -

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ def load_from_json_file(filename):
     """ creates a pthion object from a JSON file"""
 
     with open(filename, "r", encoding="utf-8") as file:
-        return json.loads(file)
+        return json.load(file)
