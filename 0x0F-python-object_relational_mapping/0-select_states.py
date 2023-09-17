bc559e0ef857a2b3a026d3743eb31e27e86ea22f
sarahassan0 <sarraree5@gmail.com>
2023-09-17 07:22:48 +0300

    s

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,25 +6,22 @@ import MySQLdb
 from sys import argv as args
 
 if __name__ == "__main__":
-    try:
-        mysql_username, mysql_password, mysql_db_name = args[1],\
-            args[2], args[3]
-        db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=mysql_db_name
-        )
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        db.close()
-    except MySQLdb.Error as err:
-        print("MySQL Error:", err)
+    mysql_username, mysql_password, mysql_db_name = args[1],\
+        args[2], args[3]
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_db_name
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
 
 # useing context manager the "with" to manage the closing of the DB conection
 #  instead of closing it manually
