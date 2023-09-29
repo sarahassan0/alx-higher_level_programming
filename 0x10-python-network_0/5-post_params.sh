71af528349fbf320c558c15c394c4b6c5351ec55
sarahassan0 <sararee5@gmail.com>
2023-09-29 05:20:44 +0300

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the size of the body of the response to the URL using Curl.
-curl -LdX POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sLX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
