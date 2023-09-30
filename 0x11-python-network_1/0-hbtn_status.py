ab0d9c6c02c33053e351806a749416864b96c666
sarahassan0 <sararee5@gmail.com>
2023-09-30 04:05:01 +0300

    1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Python script that fetches https://alx-intranet.hbtn.io/status
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 if __name__ == "__main__":
     from urllib import request
