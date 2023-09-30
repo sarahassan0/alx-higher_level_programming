10279294cdff058796d7f13e49cc6a3c9c31038c
sarahassan0 <sararee5@gmail.com>
2023-09-30 03:59:47 +0300

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # Python script that fetches https://alx-intranet.hbtn.io/status
 
-from urllib import request
+if __name__ == "__main__":
+    from urllib import request
 
-with request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-    body = res.read()
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        body = res.read()
 
-    print('Body response:')
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf8 content: {body.decode('UTF-8')}")
+        print('Body response:')
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('UTF-8')}")
