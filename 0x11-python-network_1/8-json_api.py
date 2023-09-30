8161d5e2033bb64dc40bd68013d898622319bce9
sarahassan0 <sararee5@gmail.com>
2023-09-30 06:02:46 +0300

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""script that takes in a letter and send a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    data = {'q': argv[1] if len(argv) > 1 else ""}
+    
+    with requests.post('http://0.0.0.0:5000/search_user', data=data) as res:
+        try:
+            res = res.json()
+            if len(res):
+                print(f'[{res.get("id")}] {res.get("name")}')
+            else:
+                print('No result')
+        except ValueError:
+            print('Not a valid JSON')
