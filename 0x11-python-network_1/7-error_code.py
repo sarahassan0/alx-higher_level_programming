b5f6ee9145177da436564fa2af2ef7b051dc501c
sarahassan0 <sararee5@gmail.com>
2023-09-30 05:55:30 +0300

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" script that takes in a URL, sends a request to the URL and displays the
-    value of the X-Request-Id variable found in the header of the response."""
+""" script that takes in a URL, sends a request to the URL and
+displays the body of the response.
+If the HTTP status code is greater than or equal to 400, print it """
 
 if __name__ == "__main__":
     import requests
