9a53d500ca2ecca19506879cfa317099aea86c89
sarahassan0 <sarraree5@gmail.com>
2023-10-24 18:06:47 -0700

    Task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('code: ' + response.statusCode);
+});
