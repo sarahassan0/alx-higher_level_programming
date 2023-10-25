29a4d1873320c512b77ca3c98d7b1368cba73040
sarahassan0 <sarraree5@gmail.com>
2023-10-24 18:07:14 -0700

    Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  const chars = data.results.filter(f => f.characters.find(c => c.includes('18')));
+  console.log(chars.length);
+});
